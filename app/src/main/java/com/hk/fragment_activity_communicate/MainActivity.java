fe3d75e81376148d51c2d332b3ed60a1f62f91ea
hkobir <hkobir10@gmail.com>
2022-01-28 19:06:29 +0600

    initial commited

---
+++ app/src/main/java/com/hk/fragment_activity_communicate/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hk.fragment_activity_communicate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
